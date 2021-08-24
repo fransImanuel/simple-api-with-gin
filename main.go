46ce17a30af161e4e0616925335a52f4bf8a1bad
fransimanue <frans.imanuel@student.umn.ac.id>
2021-08-24 23:15:09 +0700

    trying to connect with postgre

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"go-simple-api/handler"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 )
 
 
 func main() {
+	conn, err := pgx.Connect(context.Background(), "user=jack password=secret host=pg.example.com port=5432 dbname=mydb sslmode=verify-ca pool_max_conns=10")
+
 	router := gin.Default()
 
 	router.GET("/", handler.FirstLanding)
